63196d57b3ca7017f18aac35c07443a780ac31f8
MichelleFragosoC <michellefragosoc@outlook.com>
2021-03-02 13:25:03 -0600

    Cambios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("Raul Adri Carlos")
+import perritos from './data.js'
+
+let elDOM=document.getElementById("app")
+let perritohtml
+
+perritos.dogs.forEach{unperrito=>{
+    perritohtml+=setHtml(unperrito)
+    elDOM.innerHTML=perritohtml
+//    console.log(unperrito.breed, "Esto es verdad")
+}}
+
+function setHtml(unperro){
+    let html=`<img src="${unperro.image}"/>
+            <p>${unperro.breed}</p>
+            `
+    return html
+}
